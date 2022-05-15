9cc301ede8ae802a118af2fadb74946c4b37f086
lz-chen <lingzhen.chen@inmeta.no>
2022-05-15 17:16:13 +0200

    add testing

--- code/test_predict.py
+++ code/test_predict.py
@@ -4,14 +4,17 @@ import json
 import requests
 
 
-def main(endpoint):
+def main(endpoint, key):
     # An array of new data cases
     print("Creating input data")
     x_new = [[0.1, 2.3, 4.1, 2.0], [0.2, 1.8, 3.9, 2.1]]
     # Convert the array to a serializable list in a JSON document
     json_data = json.dumps({"data": x_new})
     # Set the content type in the request headers
-    request_headers = {"Content-Type": "application/json"}
+    request_headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + key,
+    }
 
     # Call the service
     response = requests.post(url=endpoint, data=json_data, headers=request_headers)
@@ -21,11 +24,12 @@ def main(endpoint):
 
     # Print the predicted class for each case.
     for i in range(len(x_new)):
-        print((x_new[i]), predictions[i])
+        print(f"data: {x_new[i]}; prediction: {predictions[i]}")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--scoring-url", type=str, help="Scoring URL")
+    parser.add_argument("--primary-key", type=str, help="Auth key")
     args = parser.parse_args()
-    main(endpoint=args.scoring_url)
+    main(endpoint=args.scoring_url, key=args.primary_key)
